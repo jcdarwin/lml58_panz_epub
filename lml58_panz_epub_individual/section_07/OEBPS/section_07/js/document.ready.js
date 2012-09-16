9c3735e334cdba071a0f699b3ebd6942e31c3681
Jason Darwin <jcdarwin@gmail.com>
2012-09-16 22:11:12 +1200

    Reposition the jsplayer controls so they appear correctly on iPad

--- lml58_panz_epub_individual/section_07/OEBPS/section_07/js/document.ready.js
+++ lml58_panz_epub_individual/section_07/OEBPS/section_07/js/document.ready.js
@@ -32,9 +32,23 @@ $(document).ready(function(){
 	});
 
 	$("a.jp-full-screen").bind('click', function(event){
+		/* Safari on iPad display requires full screen controls moved up. */
+		var isiPad = /ipad/i.test(navigator.userAgent.toLowerCase());
+		if (isiPad) {
+    	$('div.jp-video-full div.jp-interface').css('bottom', '88px');
+		}
+
 		/* Make sure the controls show immediately upon transition to full-screen */
 		/* as tablets don't have mousemove. */
-		$("div#jquery_jplayer_1").trigger('mousemove');		
+		$("div#jquery_jplayer_1").trigger('mousemove');
+	});
+
+	$("a.jp-restore-screen").bind('click', function(event){
+		/* Restore controls to rightful place for Safari on iPad. */
+		var isiPad = /ipad/i.test(navigator.userAgent.toLowerCase());
+		if (isiPad) {
+    	$('div.jp-video div.jp-interface').css('bottom', '0px');
+		}
 	});
 
 	$("div.jp-video-play").bind('click', function(event){
